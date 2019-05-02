bfcdca8b57576694bfe7303d09ab5da23a0b9441
reneecosmic <renee.hickman@outlook.com>
2019-05-01 19:55:07 -0500

    models defined, routes configured, controller methods

--- models/book.js
+++ models/book.js
@@ -2,10 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  title: { 
+    type: String, 
+    required: true 
+  },
+  authors: { 
+    type: String, 
+    required: true 
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  link: {
+    type: String,
+    required: true
+  }
 });
 
 const Book = mongoose.model("Book", bookSchema);
