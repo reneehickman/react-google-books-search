98f4996cc47692d045e13109c9deca911b7ef666
reneecosmic <renee.hickman@outlook.com>
2019-05-04 14:15:41 -0500

    deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 // set up mongoose to leverage built-in JavaScript ES6 Promises
