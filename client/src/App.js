f971d3e3c7938cdf69017fcf76bb5ba2e5cbb994
reneecosmic <renee.hickman@outlook.com>
2019-05-04 14:20:05 -0500

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
+import Search from "./pages/search";
+import Saved from "./pages/saved";
 import Footer from "./components/Footer";
 import Navi from "./components/Header/Navi";
 import Header from "./components/Header";
